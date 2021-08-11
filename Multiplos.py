bee3dc88e7c51464d3720476c083b21e95e0c51a
NiiSV811 <sarmiento.vargasnicolas.8@gmail.com>
2021-08-10 19:52:51 -0500

    Ejercicio 2

--- Multiplos.py
+++ Multiplos.py
@@ -9,9 +9,5 @@ def Multiplo(a, b):
 
 print("Programa para evaluar si el primer numero es multiplo del segundo...")
 
-#Ingreso de datos
-a= int(input("Ingrese el primer numero: "))
-b= int(input("Ingrese el segundo numero: "))
-
 #Llamamos la funcion
-Multiplo(a,b)
+Multiplo(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")))
