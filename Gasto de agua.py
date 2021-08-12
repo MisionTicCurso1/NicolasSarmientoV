77c29996a51f22d616c1e016589297f2c02826f2
NiiSV811 <sarmiento.vargasnicolas.8@gmail.com>
2021-08-11 22:22:19 -0500

    Ejercicio 4

---
+++ Gasto de agua.py
@@ -0,0 +1,33 @@
+#Ejercicio para cobrar el consumo de H2O
+print(".........Cobro de agua por m^3............")
+a= int(input("Ingrese su consumo de agua en m^3: "))
+
+#Cobrar los primeros 100
+if a>100:
+    a-=100
+    cobro=100*10
+else:
+    cobro=a*10
+
+#Descomponemos el valor en unidades m, centenas, decenas y unidades
+um = a//1000
+a-= (um*1000)
+c= a//100
+a-= (c*100)
+d= a//10
+a-= (d*10)
+u=a
+
+#Evaluamos los valores descompuestos
+
+if um>=1:
+    um*=1000
+    cobro+=um*90
+if 1<=c<=5:
+    c= (c*100)+(d*10)+ u
+    cobro+=c*25
+if 5<c<=9:
+    c= (c*100)+(d*10)+ u
+    cobro+=c*50
+    
+print("El total a pargar por su consumo es de $",cobro)
